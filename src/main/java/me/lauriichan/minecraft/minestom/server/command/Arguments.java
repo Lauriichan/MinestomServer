65baea31601c482337046bfcc0f98b7f9ce1ca07
Lauriichan <lauriichan@playuniverse.org>
2024-12-26 15:05:49 +0100

    Remove unnecessary class

--- src/main/java/me/lauriichan/minecraft/minestom/server/command/Arguments.java
+++
@@ -1,9 +0,0 @@
-package me.lauriichan.minecraft.minestom.server.command;
-
-public final class Arguments extends net.minestom.server.command.builder.arguments.ArgumentType {
-
-    private Arguments() {
-        throw new UnsupportedOperationException();
-    }
-
-}
